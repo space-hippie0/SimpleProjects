87d31c3dab70fb5568692f110da4fa643a1db78e
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-06-04 11:21:46 +0200

    Update particle.h

--- ParticleSimulation/particle.h
+++ ParticleSimulation/particle.h
@@ -1,1 +1,31 @@
+#ifndef PARTICLE_H
+#define PARTICLE_H
 
+#include <stdio.h>
+
+#define NUM_COLORS 7
+
+typedef struct {
+    float x;
+    float y;
+    float prev_x;
+    float prev_y;
+    float radius;
+    float mass;
+    float x_vel;
+    float y_vel;
+    int color_index;
+    float color_r;
+    float color_g;
+    float color_b;
+} Particle;
+
+
+#define RADIUS 10.0f
+
+void initialize_particle(Particle *particle, float width, float height, int index); // Updated to initialize a single particle
+void update_particles(Particle particles[], int num_particles, float dt, float width); // Added width parameter
+void handle_collisions(Particle particles[], int num_particles, float width, float height);
+void output_particles_to_csv(Particle particles[], int num_particles, FILE *file);
+
+#endif // PARTICLE_H
