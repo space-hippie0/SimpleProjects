14f7fd32153ec2eac18c72bc887d350b4a541819
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-08-25 12:43:04 +0300

    Update main.js

--- JavaScript/1/main.js
+++ JavaScript/1/main.js
@@ -1,1 +1,13 @@
+function getAverage(scores) {
+  let sum = 0;
+  
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+  
+  let average = sum / scores.length;
+  return average;
+}
 
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
