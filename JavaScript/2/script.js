5a0929c1eec80bd88b442da6cbd05d7013a2cb95
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-09-03 18:41:47 +0300

    Update script.js

--- JavaScript/2/script.js
+++ JavaScript/2/script.js
@@ -159,17 +159,27 @@ function goFight() {
   monsterHealthText.innerText = monsterHealth;
 }
 
-
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeaponIndex].power;
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
+}
+
+function dodge() {
 
 }
 
+function defeatMonster(){
 
-
-function dodge() {
+}
+function lose(){
 
 }
