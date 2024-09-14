6dd941c5f8562047fd40c436c0d73442fbc211b6
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-09-14 16:17:16 +0200

    Update script.js

--- JavaScript/4/script.js
+++ JavaScript/4/script.js
@@ -31,3 +31,12 @@ function addEntry() {
   />`;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+  }
+}
