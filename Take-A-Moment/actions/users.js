22fc923b9fd03bec9a7321162ebf098ceacdaeb2
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-04 17:49:02 +0200

    Update users.js

--- Take-A-Moment/actions/users.js
+++ Take-A-Moment/actions/users.js
@@ -1,1 +1,25 @@
+"use server"
+import {auth, clerkClient} from "@clerk/nextjs/server";
+import {db} from "@/lib/prisma";
+
+export async function updateUsername(username){
+    const {userId} = auth();
+    if (!userId) {
+        throw new Error("Unauthorized");
+    }
+    const existingUsername = await db.user.findUnique({
+        where: {username},
+    })
+    if (existingUsername && existingUsername.id !== userId){
+        throw new Error("Username is taken!");
+    }
+    await db.user.update({
+        where: {clerkUserId: userId},
+        data: {username},
+    });
+    await clerkClient.users.updateUser(userId, {
+        username,
+    });
+    return {success:true};
+}
 
