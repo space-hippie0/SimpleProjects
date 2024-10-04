5f26cdc984a9c6557c95713857f628c3af26a602
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-04 17:51:42 +0200

    Update prisma.js

--- Take-A-Moment/lib/prisma.js
+++ Take-A-Moment/lib/prisma.js
@@ -1,1 +1,9 @@
+import {PrismaClient} from "@prisma/client";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production"){
+    globalThis.prisma = db;
+}
