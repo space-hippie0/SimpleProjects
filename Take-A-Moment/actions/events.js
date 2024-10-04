4dca62e21225c685286c31855d5a789b2b573325
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-04 17:48:48 +0200

    Update events.js

--- Take-A-Moment/actions/events.js
+++ Take-A-Moment/actions/events.js
@@ -1,1 +1,31 @@
+"use server"
+
+import {auth} from "@clerk/nextjs/server";
+import {eventSchema} from "@/app/lib/validators";
+import {db} from "@/lib/prisma";
+
+export async function createEvent(data){
+    const {userId} = auth();
+    if (!userId){
+        throw new Error("Unauthorized");
+    }
+
+    const validatedData = eventSchema.parse(data);
+
+    const existingUser = await db.user.findUnique({
+        where: {clerkUserId: userId},
+    });
+
+    if (!existingUser) {
+        throw new Error("user not found !");
+    }
+
+    const event = await db.event.create({
+        data: {
+            ...validatedData,
+            userId: existingUser.id,
+        },
+    });
+    return event;
+}
 
