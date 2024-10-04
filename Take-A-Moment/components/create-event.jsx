1a23f70cabd9af88b852d6c0e7b81b2fc9402a66
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-04 17:50:10 +0200

    Update create-event.jsx

--- Take-A-Moment/components/create-event.jsx
+++ Take-A-Moment/components/create-event.jsx
@@ -1,1 +1,60 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+} from "@/components/ui/drawer"
+import {useEffect, useState} from "react";
+import {useSearchParams, useRouter} from "next/navigation";
+import EventForm from "@/components/event-form";
+
+
+export default function CreateEventDrawer() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const create = searchParams.get("create")
+        if (create==='true'){
+            setIsOpen(true);
+        }
+    }, [searchParams]);
+
+    const handleClose = () =>{
+        setIsOpen(false)
+        if(searchParams.get("create")==="true"){
+            router.replace(window.location?.pathname)
+        }
+    };
+
+    return (
+        <Drawer open={isOpen} onClose={handleClose}>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Create New Event</DrawerTitle>
+                </DrawerHeader>
+                <EventForm
+                onSubmitForm={()=>{
+                    handleClose();
+                }}
+                />
+                <DrawerFooter>
+                    <DrawerClose asChild>
+                        <Button variant="outline" onClick={handleClose}>
+                            Cancel
+                        </Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    )
+}
 
