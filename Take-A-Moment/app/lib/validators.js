0a9ca85f6b1bc76ffa179d6e8fa0f15e4cf910ef
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-07 11:44:39 +0200

    Update validators.js

--- Take-A-Moment/app/lib/validators.js
+++ Take-A-Moment/app/lib/validators.js
@@ -1,1 +1,24 @@
+import {z} from "zod";
 
+export const usernameSchema = z.object({
+    username: z
+        .string()
+        .min(2)
+        .max(20)
+        .regex(/^[a-zA-Z0-9_]+$/,
+            "Username should only contain letters, numbers, underscores"),
+});
+
+export const eventSchema = z.object({
+    title: z
+        .string()
+        .min(1, "needs a name !")
+        .max(100, "a little overkill? <100 !"),
+    description: z
+        .string()
+        .min(1, "needs a description !")
+        .max(500, "are you writing a novel? <500 !"),
+    duration: z.number().int().positive("duration must not bend time !"),
+    isPrivate: z.boolean(),
+
+});
