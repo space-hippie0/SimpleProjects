e3becbd5fdb1dfed09a3946259a9c732f3ac5616
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-10 17:29:24 +0200

    Update layout.jsx

--- Take-A-Moment/app/(main)/availability/layout.jsx
+++ Take-A-Moment/app/(main)/availability/layout.jsx
@@ -1,1 +1,10 @@
+import {Suspense} from "react";
+
+export default function AvailabilityLayout({children}) {
+    return (
+        <div className="mx-auto">
+            <Suspense fallback={<div>Loading Availability...</div>}>{children}</Suspense>
+        </div>
+    );
+}
 
