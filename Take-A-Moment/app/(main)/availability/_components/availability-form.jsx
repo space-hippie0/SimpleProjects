6833bff6d0d650b7f4d5b049e03328d74fa3fe00
space-hippie0 <118982314+space-hippie0@users.noreply.github.com>
2024-10-11 14:43:07 +0200

    Update availability-form.jsx

--- Take-A-Moment/app/(main)/availability/_components/availability-form.jsx
+++ Take-A-Moment/app/(main)/availability/_components/availability-form.jsx
@@ -1,1 +1,137 @@
+"use client"
 
+import {Controller, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {availabilitySchema} from "@/app/lib/validators";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
+import {timeSlots} from "@/app/(main)/availability/data";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+
+
+const AvailabilityForm = ({initialData}) => {
+
+    const {register, handleSubmit,control, setValue, watch, formState:{errors}} = useForm({
+        resolver:zodResolver(availabilitySchema),
+        defaultValues:{...initialData},
+    });
+
+    return <form>
+        {[
+            "monday",
+            "tuesday",
+            "wednesday",
+            "thursday",
+            "friday",
+            "saturday",
+            "sunday",
+        ].map((day)=>{
+            const isAvailable = watch(`${day}.isAvailable`);
+            return(
+                <div key={day} className="flex items-center space-x-4 mb-4">
+                    <Controller
+                        name={`${day}.isAvailable`}
+                        control={control}
+                        render={({field})=>{
+                            return (
+                                <Checkbox
+                                    checked={field.value}
+                                    onCheckedChange={(checked)=>{
+                                        setValue(`${day}.isAvailable`, checked);
+                                        if (!checked) {
+                                            setValue(`${day}.startTime`, "09:00");
+                                            setValue(`${day}.endTime`, "17:00");
+                                        }
+                                    }}
+                                />
+                            );
+                        }}
+                    />
+
+
+
+                    <span className="w-24">
+                        {day.charAt(0).toUpperCase() + day.slice(1)}
+                    </span>
+
+                    {isAvailable && (
+                        <>
+                            <Controller
+                                name={`${day}.startTime`}
+                                control={control}
+                                render={({field})=>{
+                                    return (
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                        >
+                                            <SelectTrigger className="w-32">
+                                                <SelectValue placeholder="Starting at: " />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                {timeSlots.map(time=>{
+                                                    return <SelectItem key={time} value={time}>{time}</SelectItem>
+                                                })}
+                                            </SelectContent>
+                                        </Select>
+
+                                    );
+                                }}
+                            />
+                            <span>to</span>
+                            <Controller
+                                name={`${day}.endTime`}
+                                control={control}
+                                render={({field})=>{
+                                    return (
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            value={field.value}
+                                        >
+                                            <SelectTrigger className="w-32">
+                                                <SelectValue placeholder="Ending at: " />
+                                            </SelectTrigger>
+                                            <SelectContent>
+                                                {timeSlots.map(time=>{
+                                                    return <SelectItem key={time} value={time}>{time}</SelectItem>
+                                                })}
+                                            </SelectContent>
+                                        </Select>
+
+                                    );
+                                }}
+                            />
+                            {errors[day]?.endTime && (
+                                <span className="text-red-500 text-sm ml-2">
+                                    {errors[day].endTime.message}
+                                </span>
+                            )}
+                        </>
+                    )}
+                </div>
+            )
+        })}
+
+        <div className="flex items-center space-x-4">
+            <span className="w-48">Minimum gap before booking (minutes):</span>
+            <Input
+                type="number"
+                {...register("timeGap", {
+                    valueAsNumber: true,
+                })}
+                className="w-32"
+            />
+            {errors?.timeGap && (
+                <span className="text-red-500 text-sm ml-2">
+                    {errors.timeGap.message}
+                </span>
+            )}
+        </div>
+
+        <Button type="submit" className="mt-5" >Update Availability</Button>
+
+    </form>
+};
+
+export default AvailabilityForm;
