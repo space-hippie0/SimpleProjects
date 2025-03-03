844c071deb76f73e72ec554f88b70bde2097b96f
space-hippie0 <mertakoz@gmail.com>
2025-03-03 10:07:13 +0100

    added Java

---
+++ Java/firstProgram/src/Bicycle.java
@@ -0,0 +1,15 @@
+public class Bicycle {
+
+    // attributes
+    private String wheelType;
+    private int modelNumber;
+    private String color;
+    private String productionDate;
+
+    // methods
+    public String getColor(){
+        return color;
+    }
+
+    
+}
